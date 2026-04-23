5c87a500eb12e7d4586c169ab2e4174737e54cf3
Emil2001 <serazdinovemil@gmail.com>
2026-04-23 21:26:55 +0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,24 @@
+# config.py
+
+import pygame
+
+# --- Системные ---
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+FPS = 60
+GAME_TITLE = "Смотритель Перекрёстка"
+
+# --- Цвета ---
+COLOR_BACKGROUND = (13, 27, 42)       # #0D1B2A
+COLOR_PANEL = (27, 40, 56)            # #1B2838
+COLOR_ACCENT = (244, 211, 94)         # #F4D35E
+COLOR_HIGHLIGHT = (168, 218, 220)     # #A8DADC
+COLOR_DANGER = (230, 57, 70)          # #E63946
+COLOR_TEXT = (241, 250, 238)          # #F1FAEE
+
+# --- Пути ---
+DATA_PATH = "data/"
+ASSETS_PATH = "assets/"
+FONTS_PATH = ASSETS_PATH + "fonts/"
+IMAGES_PATH = ASSETS_PATH + "images/"
+SOUNDS_PATH = ASSETS_PATH + "sounds/"
